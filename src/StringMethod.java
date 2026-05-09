99afe99fa7b306ffa9e9b4bf8deab0ff8008736e
Madhu Kushwaha <madhu.kushwaha9005@gmail.com>
2026-05-09 19:38:18 +0530

    Strings Methods

---
+++ src/StringMethod.java
@@ -0,0 +1,46 @@
+public class StringMethod {
+
+    static void main() {
+
+//        Practice Questions
+//• Print each character of a String
+//• Count the length of a String without using length()
+//• Count vowels in a String
+//• Reverse a String
+//• Check whether a String is palindrome or not
+
+        //String str = " Hello Java ";
+//
+//        System.out.println(str.length());          // 12
+//        System.out.println(str.charAt(1));        // H
+//        System.out.println(str.substring(1, 6));  // Hello
+//        System.out.println(str.contains("Java")); // true
+//        System.out.println(str.equals("Hello"));  // false
+//        System.out.println(str.toUpperCase());    // HELLO JAVA
+//        System.out.println(str.toLowerCase());    // hello java
+//        System.out.println(str.trim());           // Hello Java
+//        System.out.println(str.startsWith(" "));  // true
+//        System.out.println(str.endsWith(" "));    // true
+//        System.out.println(str.isEmpty());        // false
+//        System.out.println(str.replace('J','K')); // Hello Kava
+    }
+}
+
+
+//        1. length()
+//        2. charAt(int index)
+//        3. substring(int beginIndex, int endIndex)
+//        4. contains(CharSequence s)
+//        5. equals(Object o)
+//        6. equalsIgnoreCase(String s)
+//        7. toUpperCase()
+//        8. toLowerCase()
+//        9. trim()
+//        10. split(String regex)
+//        11. startsWith(String prefix)
+//        12. endsWith(String suffix)
+//        13. valueOf(any type)
+//        14. toCharArray()
+//        15. isEmpty()
+//        16. isBlank()
+//        17. replace(char oldChar, char newChar)
