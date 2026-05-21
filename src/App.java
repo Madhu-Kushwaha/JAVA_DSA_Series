26f2556dc482befebc17bd6b64d76c9ac0996a2a
Madhu Kushwaha <madhu.kushwaha9005@gmail.com>
2026-05-21 17:49:21 +0530

    Encapsulation in Java

--- src/App.java
+++ src/App.java
@@ -19,15 +19,21 @@ public class App {
 
        //Parmetrized constructor
 
-       Student A = new Student(1,12,"Rahul",3);
+       Student A = new Student(1,18,"Rahul",3,"Tina");
 
-       System.out.println(A.id);
-       System.out.println(A.age);
-       System.out.println(A.name);
-       System.out.println(A.nos);
+//       System.out.println(A.id);
+//       System.out.println(A.age);
+//       System.out.println(A.name);
+//       System.out.println(A.nos);
+//       System.out.println(A.gf);  private data encapsulation
 
-       A.bunk();
-       A.sleep();
-       A.study();
+//       System.out.println(A.getName());
+       System.out.println(A.getAge());
+       A.setAge(67);
+       System.out.println(A.getAge());
+//       A.bunk();
+//       A.sleep();
+//       A.study();
+//       A.gfChatting();
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,9 +1,33 @@
+import java.sql.SQLOutput;
+
 public class Student {
     //Attributes
-    public int id;
-    public int age;
-    public String name;
-    public int nos;
+//    public int id;
+//    public int age;
+//    public String name;
+//    public int nos;
+//    private String gf;
+
+//   Perfecatly encapsulation
+    private int id;
+    private int age;
+    private String name;
+    private int nos;
+    private String gf;
+
+  //a layer through we can access the data
+    public String getName () {
+        return this.name;
+    }
+    public int getAge(){
+        return this.age;
+    }
+    public void setAge(int a){
+       //extra layer of authentication
+        if(age<100)
+            this.age = a;
+        return;
+    }
 
     //Default ctor  /// attr -> garbage
     public Student() {
@@ -11,12 +35,13 @@ public class Student {
     }
 
     //Parametrized ctor
-    public Student(int id,int age, String name,int nos){
+    public Student(int id,int age, String name,int nos,String gf){
         System.out.println("Student Parametrized ctor called");
         this.id = id;
         this.age = age;
         this.name = name;
         this.nos = nos;
+        this.gf=gf;
     }
     public void study() {
         System.out.println(name +" Studying");
@@ -26,5 +51,8 @@ public class Student {
     }
     public void bunk() {
         System.out.println(name +" bunk");
+    }
+    private void gfChatting(){
+        System.out.println(name + "gfChatting");
     }
 }
